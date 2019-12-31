3f2881b6ec690c93b27c557674c624cf6e988628
Cristian Arranz Arevalo <carranz@pclxit18.esmaddomw2k.selftrade.es>
2019-12-31 14:13:40 +0100

    Commit - Add menu in experience

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { KnowMeComponent } from '../know-me/know-me.component';
 import { ConcernsComponent } from '../know-me/concerns/concerns.component';
 import { HobbiesComponent } from '../know-me/hobbies/hobbies.component';
 import { InterestsComponent } from '../know-me/interests/interests.component';
@@ -8,11 +9,13 @@ import { KnowledgeComponent } from '../know-me/knowledge/knowledge.component';
 import { LikesComponent } from '../know-me/likes/likes.component';
 
 const appRoutes: Routes = [
-  { path: 'concerns', component: ConcernsComponent },
-  { path: 'hobbies', component: HobbiesComponent },
-  { path: 'interests', component: InterestsComponent },
-  { path: 'knowledge', component: KnowledgeComponent },
-  { path: 'likes', component: LikesComponent }
+  { path: 'knowme', component: KnowMeComponent, children: [
+    { path: 'concerns', component: ConcernsComponent},
+    { path: 'hobbies', component: HobbiesComponent },
+    { path: 'interests', component: InterestsComponent },
+    { path: 'knowledge', component: KnowledgeComponent },
+    { path: 'likes', component: LikesComponent }
+  ]}
 ];
 
 @NgModule({
