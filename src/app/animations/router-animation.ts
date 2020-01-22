a39d45c5622d8ba0d5ed13df029e0996b51be28c
Cristian Arranz Arevalo <carranz@pclxit18.esmaddomw2k.selftrade.es>
2020-01-22 16:58:57 +0100

    Commit - Fix error in animation and fix responsive elements

--- src/app/animations/router-animation.ts
+++ src/app/animations/router-animation.ts
@@ -11,10 +11,10 @@ export const slideInAnimation =
           opacity: 0,
           transform: 'scale(0) translateY(100%)'
         })
-      ]),
+      ], { optional: true }),
       query(':enter', [
         animate('600ms ease',
           style({ opacity: 1, transform: 'scale(1) translateY(0)' }))
-      ])
+      ], { optional: true })
     ])
 ]);
